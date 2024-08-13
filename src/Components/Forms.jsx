06831d17f4db74d5c50d60ba3dd49514ec1874fb
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-08-13 09:44:40 +0530

    Changed Home Page Content

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -155,13 +155,12 @@ function Forms() {
                 </svg>
               </a>
 
-              <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                Welcome to Squid 🦑
+              <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-6xl ">
+                Welcome to Sky Test 🦑
               </h2>
 
-              <p className="mt-4 leading-relaxed text-white/90">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
+              <p className="mt-4 leading-relaxed text-white md:text-xl">
+                Take this ultimate test , to test your understanding
               </p>
             </div>
           </section>
